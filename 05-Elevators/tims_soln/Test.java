e44b732f74bc875eda725f738ab2621079f342cd
timcop <timothy.copland98@gmail.com>
2021-05-25 18:16:31 +1200

    Update Test.java

--- 05-Elevators/tims_soln/Test.java
+++ 05-Elevators/tims_soln/Test.java
@@ -5,6 +5,8 @@ public class Test {
 
     public static void main(String[] args) {
 
+        // TO DO: Make ti so we can test different strats on the same
+        // customers
         Elevator e1 = new Elevator(10, 100);
         Elevator e2 = new Elevator(10, 100);
         FIFS fifs = new FIFS();
